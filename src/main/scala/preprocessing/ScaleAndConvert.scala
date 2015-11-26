f11bac83e1946ea35aaa1d245db7a4f56760ce99
Robert Nishihara <robertnishihara@gmail.com>
2015-11-26 01:40:07 -0800

    make ScaleAndConvert work with ByteNDArray

--- src/main/scala/preprocessing/ScaleAndConvert.scala
+++ src/main/scala/preprocessing/ScaleAndConvert.scala
@@ -11,14 +11,15 @@ import net.coobird.thumbnailator._
 import org.apache.spark.rdd.RDD
 
 import libs.NDArray
+import libs.ByteNDArray
 
 class ScaleAndConvert(batchsize: Int, height: Int, width: Int) extends java.io.Serializable {
-  def convertImage(compressedImg : Array[Byte]) : Option[NDArray] = {
+  def convertImage(compressedImg : Array[Byte]) : Option[ByteNDArray] = {
     val im = ImageIO.read(new ByteArrayInputStream(compressedImg))
     try {
       val resizedImage = Thumbnails.of(im).forceSize(width, height).asBufferedImage()
       val pixels = resizedImage.getRGB(0, 0, width, height, null: Array[Int], 0, width)
-      val array = new Array[Float](3 * width * height)
+      val array = new Array[Byte](3 * width * height)
       assert(3 * pixels.length == array.length, "pixels.length = " + pixels.length.toString + ", array.length = " + array.length.toString)
 
       var index = 0
@@ -27,15 +28,15 @@ class ScaleAndConvert(batchsize: Int, height: Int, width: Int) extends java.io.S
         var col = 0
         while (col < width) {
           val rgb = pixels(row * width + col)
-          array(0 * width * height + row * width + col) = (rgb >> 16) & 0xFF // red
-          array(1 * width * height + row * width + col) = (rgb >> 8) & 0xFF // green
-          array(2 * width * height + row * width + col) = (rgb) & 0xFF // blue
+          array(0 * width * height + row * width + col) = ((rgb >> 16) & 0xFF).toByte // red
+          array(1 * width * height + row * width + col) = ((rgb >> 8) & 0xFF).toByte // green
+          array(2 * width * height + row * width + col) = ((rgb) & 0xFF).toByte // blue
           col += 1
         }
         row += 1
       }
 
-      Some(NDArray(array, Array[Int](3, width, height)))
+      Some(ByteNDArray(array, Array[Int](3, width, height)))
     } catch {
       // If images can't be processed properly, just ignore them
       case e: java.lang.IllegalArgumentException => None
@@ -45,13 +46,13 @@ class ScaleAndConvert(batchsize: Int, height: Int, width: Int) extends java.io.S
   }
 
   // This method will drop examples so that the number of training examples is divisible by the batch size
-  def makeMinibatchRDD(data: RDD[(Array[Byte], Int)]) : RDD[(Array[NDArray], Array[Int])] = {
+  def makeMinibatchRDD(data: RDD[(Array[Byte], Int)]) : RDD[(Array[ByteNDArray], Array[Int])] = {
     data.mapPartitions(
       it => {
-        val accumulator = new ArrayBuffer[(Array[NDArray], Array[Int])]
+        val accumulator = new ArrayBuffer[(Array[ByteNDArray], Array[Int])]
         // loop over minibatches
         while (it.hasNext) {
-          val imageMinibatchAccumulator = new ArrayBuffer[NDArray]
+          val imageMinibatchAccumulator = new ArrayBuffer[ByteNDArray]
           val labelMinibatchAccumulator = new ArrayBuffer[Int]
           while (it.hasNext && imageMinibatchAccumulator.length != batchsize) {
             val (compressedImg, label) = it.next
@@ -72,7 +73,7 @@ class ScaleAndConvert(batchsize: Int, height: Int, width: Int) extends java.io.S
     )
   }
 
-  def apply(data: RDD[(Array[Byte], Int)]) : RDD[(Array[NDArray], Array[Int])] = {
+  def apply(data: RDD[(Array[Byte], Int)]) : RDD[(Array[ByteNDArray], Array[Int])] = {
     makeMinibatchRDD(data)
   }
 }
