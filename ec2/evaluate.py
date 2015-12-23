77e76a15f8af4626cf4eae36acf3c7802c030c36
Philipp Moritz <pcmoritz@gmail.com>
2015-12-22 16:50:42 -0800

    update evaluation script

--- ec2/evaluate.py
+++ ec2/evaluate.py
@@ -7,9 +7,22 @@ caffe_root = '/root/caffe/'
 model = '/imagenet2/multigpu/multigpu-0.caffemodel'
 
 caffe.set_mode_gpu()
+caffe.set_device(1)
+
 net = caffe.Net(caffe_root + 'models/bvlc_googlenet/train_val.prototxt',
                 model,
                 caffe.TEST)
+
+test_iters = 1000
+accuracy = 0
+
+for it in arange(test_iters):
+    net.forward()
+    accuracy += net.blobs['loss1/top-5'].data
+
+accuracy /= test_iters
+
+print 'Accuracy:', accuracy
 
 import IPython
 IPython.embed()
