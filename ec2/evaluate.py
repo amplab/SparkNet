d99f17eda77211c1b3932216de8318f769ee0955
Philipp Moritz <pcmoritz@gmail.com>
2015-12-22 16:23:00 -0800

    add evaluation script

---
+++ ec2/evaluate.py
@@ -0,0 +1,15 @@
+# Evaluate a trained model
+
+import caffe
+
+caffe_root = '/root/caffe/'
+
+model = '/imagenet2/multigpu-0.caffemodel'
+
+caffe.set_mode_gpu()
+net = caffe.Net(caffe_root + 'models/bvlc_googlenet/train_val.prototxt',
+                model,
+                caffe.TEST)
+
+import IPython
+IPython.embed()
