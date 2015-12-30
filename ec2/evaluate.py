fa73ade5273326d8fbc25e1ae1b2163864ebacd9
Philipp Moritz <pcmoritz@gmail.com>
2015-12-30 13:37:52 -0800

    make test script more general

--- ec2/evaluate.py
+++ ec2/evaluate.py
@@ -5,30 +5,36 @@ import caffe
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("index", type=int)
+parser.add_argument("net", type=str, help="path to the train_val.prototxt file")
+parser.add_argument("prefix", type=str)
+parser.add_argument("dir", type=str, help="directory where the model files are stored")
+parser.add_argument("logfile", type=str, help="path where the resulting logfile will be stored")
 args = parser.parse_args()
 
 caffe_root = '/root/caffe/'
 
-model = '/imagenet2/multigpu/multigpu-' + str(args.index) + '.caffemodel'
+model = args.prefix + str(args.index) + '.caffemodel'
 
 caffe.set_mode_gpu()
 caffe.set_device(1)
 
-net = caffe.Net(caffe_root + 'models/bvlc_googlenet/train_val.prototxt',
-                model,
-                caffe.TEST)
+net = caffe.Net(args.net, model, caffe.TEST)
 
 test_iters = 1000
-accuracy = 0
+accuracy_top1 = 0
+accuracy_top5 = 0
 
 for it in range(test_iters):
     print "iteration ", it
     net.forward()
-    accuracy += net.blobs['loss1/top-5'].data
+    accuracy_top1 += net.blobs['loss1/top-1'].data
+    accuracy_top5 += net.blobs['loss1/top-5'].data
 
-accuracy /= test_iters
+accuracy_top1 /= test_iters
+accuracy_top5 /= test_iters
 
-print 'Accuracy:', accuracy
+print 'Accuracy top-1:', accuracy_top1
+print 'Accuracy top-5:', accuracy_top5
 
-with open("/imagenet2/multigpu-acc.txt", "a") as myfile:
-    myfile.write(str(args.index) + ',' + str(accuracy) + '\n')
+with open(args.logfile, "a") as myfile:
+    myfile.write(str(args.index) + ',' + str(accuracy_top1) + ',' + str(accuracy_top5) + '\n')
